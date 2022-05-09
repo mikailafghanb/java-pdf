1bc3b221783b62b2eaa8a4e15ac467f3ac35f242
afghanbaihaqy.11 <afghanbaihaqy.11@gmail.com>
2022-05-09 14:31:37 +0700

    [Featured] Swagger API Documentation

--- src/main/java/com/mandiri/JavaPdfApplication.java
+++ src/main/java/com/mandiri/JavaPdfApplication.java
@@ -2,12 +2,23 @@ package com.mandiri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableWebMvc
+@EnableSwagger2
 @SpringBootApplication
 public class JavaPdfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaPdfApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
